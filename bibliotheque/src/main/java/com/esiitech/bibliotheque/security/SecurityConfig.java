b661116e697e65010a8f7da93969cae952fa978c
Bodri-Mbg1 <mbgbodri@gmail.com>
2025-02-18 16:56:46 +0100

    last commit

--- bibliotheque/src/main/java/com/esiitech/bibliotheque/security/SecurityConfig.java
+++ bibliotheque/src/main/java/com/esiitech/bibliotheque/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/auth/login").permitAll()
                 .requestMatchers("/api/utilisateurs/**").permitAll()
                 .requestMatchers("/swagger-ui/**","/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                .requestMatchers("/api/livres/**").hasAnyRole("ADMIN", "USER")
+                .requestMatchers("/api/livres/**").hasRole("ADMIN")
                 .requestMatchers("/api/emprunts/**").hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
                 .and()
